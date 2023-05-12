6552914111fe05027b5637796d2c039db3b9a3b9
riyad007700 <133372910+riyad007700@users.noreply.github.com>
2023-05-12 20:27:39 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,44 @@
+blue='\33[94m'
+lightblue ='\033[94m'
+red ='\033[91m'
+white ='\33[97m'
+yellow ='\33[93m'
+green ='\033[1;32m'
+cyan ='\033[96m'
+end ='\033[0m'
+
+logo=green+str(""" 
+  _____  _______     __      _____  
+ |  __ \|_   _\ \   / //\   |  __ \ 
+ | |__) | | |  \ \_/ //  \  | |  | |
+ |  _  /  | |   \   // /\ \ | |  | |
+ | | \ \ _| |_   | |/ ____ \| |__| |
+ |_|  \_\_____|  |_/_/    \_\_____/ 
+                                    
+                                    
+ """)
+
+print(logo)
+
+text=green+str("""      
+
+                     Wellcome To My Kingdom
+     """)
+print(text)
+
+text=green+str(""" 
+
+    HI It's me,
+
+    My Name is RIYAD
+       
+      I am new to the "community" of developers of TCG.
+    
+      Explore Google's developer tools for AI, Cloud, app, web, and software development to help you build smarter and ship faster.
+
+      Contact 
+      Telligram   @MokaromHossainRiyad
+      E-mail      arriyad033@gmail.com
+ """)
+
+print(text)
